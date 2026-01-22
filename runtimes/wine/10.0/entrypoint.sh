6119f9ef6cfe872379c61bae9ad8ab28ab473b55
Sinned <vdmark.d@gmail.com>
2026-01-22 11:05:18 +0100

    "runtime: handle Wine 11 PID handoff and log tailing in entrypoint"

--- runtimes/wine/10.0/entrypoint.sh
+++ runtimes/wine/10.0/entrypoint.sh
@@ -92,5 +92,5 @@ done
 MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the Server
+# Run the Server 
 eval ${MODIFIED_STARTUP}
